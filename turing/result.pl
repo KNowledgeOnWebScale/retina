76c51909231d1f8107ea2322f111b76b4ec0c820
josd <josderoo@gmail.com>
2022-07-03 01:43:53 +0200

    retestibg

---
+++ turing/result.pl
@@ -0,0 +1,4 @@
+'https://josd.github.io/imagining#compute'([1,0,1,0,0,1],[1,0,1,0,1,0,"#"]).
+'https://josd.github.io/imagining#compute'([1,0,1,1,1,1],[1,1,0,0,0,0,"#"]).
+'https://josd.github.io/imagining#compute'([1,1,1,1,1,1],[1,0,0,0,0,0,0,"#"]).
+'https://josd.github.io/imagining#compute'([],[1,"#"]).
