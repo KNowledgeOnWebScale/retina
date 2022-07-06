d39ab8e78fcbfc4ce786b9a98286aeabfd9249c4
josd <josderoo@gmail.com>
2022-07-07 00:06:37 +0200

    graph using forward chaining

--- graph/graph.pl
+++ graph/graph.pl
@@ -1,5 +1,9 @@
 % Traversing graph paths
 
+:- use_module('../imagining.pl').
+
+:- dynamic('https://josd.github.io/imagining#path'/2).
+
 'https://josd.github.io/imagining#oneway'('http://example.org/ns#paris','http://example.org/ns#orleans').
 'https://josd.github.io/imagining#oneway'('http://example.org/ns#paris','http://example.org/ns#chartres').
 'https://josd.github.io/imagining#oneway'('http://example.org/ns#paris','http://example.org/ns#amiens').
@@ -11,16 +15,14 @@
 'https://josd.github.io/imagining#oneway'('http://example.org/ns#lemans','http://example.org/ns#tours').
 'https://josd.github.io/imagining#oneway'('http://example.org/ns#angers','http://example.org/ns#nantes').
 
-'https://josd.github.io/imagining#path'(A, B) :-
-    'https://josd.github.io/imagining#oneway'(A, B).
-'https://josd.github.io/imagining#path'(A, C) :-
-    'https://josd.github.io/imagining#oneway'(A, B),
-    'https://josd.github.io/imagining#path'(B, C).
+'https://josd.github.io/imagining#oneway'(A, B) => 'https://josd.github.io/imagining#path'(A, B).
+'https://josd.github.io/imagining#path'(A, B), 'https://josd.github.io/imagining#path'(B, C) => 'https://josd.github.io/imagining#path'(A, C).
 
 % query
 query('https://josd.github.io/imagining#path'(_CITY,'http://example.org/ns#nantes')).
 
 run :-
+    forward,
     query(Q),
     Q,
     writeq(Q),
---
+++ imagining.pl
@@ -0,0 +1,66 @@
+% -----------------------
+% Imagining -- Jos De Roo
+% -----------------------
+%
+% See https://josd.github.io/imagining/
+%
+
+:- use_module(library(lists)).
+:- use_module(library(terms)).
+
+:- op(1150, xfx, =>).
+
+:- dynamic(brake/0).
+:- dynamic(label/1).
+:- dynamic(pred/1).
+
+% forward chaining
+forward :-
+    (Prem => Conc),
+    Prem,
+    \+Conc,
+    labelvars(Conc),
+    astep(Conc),
+    retract(brake),
+    fail.
+forward :-
+    (   brake
+    ->  !
+    ;   assertz(brake),
+        forward
+    ).
+
+% create witnesses
+labelvars(Term) :-
+    (   retract(label(Current))
+    ->  true
+    ;   Current = 0
+    ),
+    numbervars(Term,Current,Next),
+    assertz(label(Next)).
+
+% assert new step
+astep((A, B)) :-
+    !,
+    astep(A),
+    astep(B).
+astep(A) :-
+    (   \+A
+    ->  assertz(A),
+        (   functor(A, B, _),
+            \+pred(B)
+        ->  assertz(pred(B))
+        ;   true
+        )
+    ;   true
+    ).
+
+%
+% built-ins
+%
+'https://josd.github.io/imagining#relate'(Relation, [P|Args]) :-
+    (   var(P)
+    ->  pred(P)
+    ;   true
+    ),
+    Relation =.. [P|Args].
