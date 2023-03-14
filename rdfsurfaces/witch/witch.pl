f11037c3f8c126453e8954b52cb3a29e2ae451e8
josd <josderoo@gmail.com>
2023-03-14 13:38:52 +0100

    refreshing

--- rdfsurfaces/witch/witch.pl
+++ rdfsurfaces/witch/witch.pl
@@ -1,9 +1,15 @@
 :- dynamic('<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'/2).
 
-:- discontiguous('<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'/2).
-:- discontiguous('<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'/2).
+% WOMAN(GIRL)
+'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#GIRL>', '<http://example.org/ns#WOMAN>').
 
-%[1]     BURNS(x) /\ WOMAN(x)         =>    WITCH(x)
+% FLOATS(DUCK)
+'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#DUCK>', '<http://example.org/ns#FLOATS>').
+
+% SAMEWEIGHT(DUCK,GIRL)
+'<http://example.org/ns#SAMEWEIGHT>'('<http://example.org/ns#DUCK>', '<http://example.org/ns#GIRL>').
+
+% BURNS(x) /\ WOMAN(x) => WITCH(x)
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:X'],
     (
         '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#BURNS>'),
@@ -14,10 +20,7 @@
     )
 ).
 
-%[2]    WOMAN(GIRL)
-'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#GIRL>', '<http://example.org/ns#WOMAN>').
-
-%[3]    \forall x, ISMADEOFWOOD(x)    =>    BURNS(x)
+% ISMADEOFWOOD(x) => BURNS(x)
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:X'],
     (
         '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#ISMADEOFWOOD>'),
@@ -27,7 +30,7 @@
     )
 ).
 
-%[4]    \forall x, FLOATS(x)         =>    ISMADEOFWOOD(x)
+% FLOATS(x) => ISMADEOFWOOD(x)
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:X'],
     (
         '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#FLOATS>'),
@@ -37,10 +40,7 @@
     )
 ).
 
-%[5]    FLOATS(DUCK)
-'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#DUCK>', '<http://example.org/ns#FLOATS>').
-
-%[6]    \forall x,y FLOATS(x) /\ SAMEWEIGHT(x,y) =>    FLOATS(y)
+% FLOATS(x) /\ SAMEWEIGHT(x,y) => FLOATS(y)
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:X', '_:Y'],
     (
         '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#FLOATS>'),
@@ -51,11 +51,7 @@
     )
 ).
 
-% and, by experiment
-% [7]    SAMEWEIGHT(DUCK,GIRL)
-'<http://example.org/ns#SAMEWEIGHT>'('<http://example.org/ns#DUCK>', '<http://example.org/ns#GIRL>').
-
 % query
 '<http://www.w3.org/2000/10/swap/log#onQuerySurface>'(['_:X'],
-    '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#WITCH>')
+ '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('_:X', '<http://example.org/ns#WITCH>')
 ).
