353fe4bfa168a77c036401594f6e01f4abbb7402
josd <josderoo@gmail.com>
2023-03-16 23:30:36 +0100

    not yet all facts

--- rdfsurfaces/socrates/socrates.pl
+++ rdfsurfaces/socrates/socrates.pl
@@ -1,13 +1,10 @@
 :- dynamic('<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'/2).
-:- dynamic('<http://www.w3.org/2000/01/rdf-schema#subClassOf>'/2).
 
-'<http://www.w3.org/2000/10/swap/log#onPositiveSurface>'([],
-    (   % Socrates is a human
-        '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#Socrates>', '<http://example.org/ns#Human>'),
-        % humans are mortal
-        '<http://www.w3.org/2000/01/rdf-schema#subClassOf>'('<http://example.org/ns#Human>', '<http://example.org/ns#Mortal>')
-    )
-).
+% Socrates is a human
+'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'('<http://example.org/ns#Socrates>', '<http://example.org/ns#Human>').
+
+% humans are mortal
+'<http://www.w3.org/2000/01/rdf-schema#subClassOf>'('<http://example.org/ns#Human>', '<http://example.org/ns#Mortal>').
 
 % rdfs subclass
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:A', '_:B', '_:S'],
