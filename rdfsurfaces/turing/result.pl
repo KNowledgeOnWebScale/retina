fd9ab428f4c3582810008e6f034724a92de09449
josd <josderoo@gmail.com>
2023-03-22 21:53:07 +0100

    adding Turing machine example

---
+++ rdfsurfaces/turing/result.pl
@@ -0,0 +1,5 @@
+% Processed by PH2 v1.3.0
+'<http://example.org/ns#compute>'([1,0,1,0,0,1],[1,0,1,0,1,0,"#"]).
+'<http://example.org/ns#compute>'([1,0,1,1,1,1],[1,1,0,0,0,0,"#"]).
+'<http://example.org/ns#compute>'([1,1,1,1,1,1],[1,0,0,0,0,0,0,"#"]).
+'<http://example.org/ns#compute>'([],[1,"#"]).
