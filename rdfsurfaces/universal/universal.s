a10efa14c0cb7a21ee6480d89e634bfe6361d123
josd <josderoo@gmail.com>
2023-06-29 01:19:12 +0200

    dealing with universal blogic statements

--- rdfsurfaces/universal/universal.s
+++ rdfsurfaces/universal/universal.s
@@ -1,12 +1,13 @@
 % everybody loves somebody who is lonely
 '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:A'],
     (
-     '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:B'],
-        (
-            '<http://books.example/loves>'('_:A','_:B'),
-            '<http://books.example/is>'('_:B','<http://books.example/lonely>')
+        '<http://www.w3.org/2000/10/swap/log#onNegativeSurface>'(['_:B'],
+            (
+                '<http://books.example/loves>'('_:A','_:B'),
+                '<http://books.example/is>'('_:B','<http://books.example/lonely>')
+            )
         )
-    ))
+    )
 ).
 
 % query
