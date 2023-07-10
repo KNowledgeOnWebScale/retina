260f10d1e70b6e9e77f61368a29201930e6db5cb
josd <josderoo@gmail.com>
2023-07-10 23:33:40 +0200

    refreshing

--- rdfsurfaces/universal/universal-answer.s
+++ rdfsurfaces/universal/universal-answer.s
@@ -1,2 +1,2 @@
-'<http://books.example/loves>'('<http://books.example/bob>','<http://knowledgeonwebscale.github.io/.well-known/genid/222c43ff-ab00-46f6-9608-7b5bdbb1556d#t_1>').
-'<http://books.example/is>'('<http://knowledgeonwebscale.github.io/.well-known/genid/222c43ff-ab00-46f6-9608-7b5bdbb1556d#t_1>','<http://books.example/lonely>').
+'<http://books.example/loves>'('<http://books.example/bob>','<http://knowledgeonwebscale.github.io/.well-known/genid/c60ed86b-0e79-41cf-9993-2e363d89be9b#t_1>').
+'<http://books.example/is>'('<http://knowledgeonwebscale.github.io/.well-known/genid/c60ed86b-0e79-41cf-9993-2e363d89be9b#t_1>','<http://books.example/lonely>').
