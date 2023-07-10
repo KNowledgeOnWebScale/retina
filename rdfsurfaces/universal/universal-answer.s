ee8685af753cd9f5a9629e4d34f3e6d425ee04f4
josd <josderoo@gmail.com>
2023-07-11 01:38:12 +0200

    running with trealla which checks the stack space and sets an appropriate stack depth

--- rdfsurfaces/universal/universal-answer.s
+++ rdfsurfaces/universal/universal-answer.s
@@ -1,2 +1,2 @@
-'<http://books.example/loves>'('<http://books.example/bob>','<http://knowledgeonwebscale.github.io/.well-known/genid/c60ed86b-0e79-41cf-9993-2e363d89be9b#t_1>').
-'<http://books.example/is>'('<http://knowledgeonwebscale.github.io/.well-known/genid/c60ed86b-0e79-41cf-9993-2e363d89be9b#t_1>','<http://books.example/lonely>').
+'<http://books.example/loves>'('<http://books.example/bob>','<http://knowledgeonwebscale.github.io/.well-known/genid/d582b69a-346f-45e9-90f9-e4512485561e#t_1>').
+'<http://books.example/is>'('<http://knowledgeonwebscale.github.io/.well-known/genid/d582b69a-346f-45e9-90f9-e4512485561e#t_1>','<http://books.example/lonely>').
